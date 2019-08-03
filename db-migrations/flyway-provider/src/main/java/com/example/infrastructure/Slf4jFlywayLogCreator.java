a468300d3790799fdcd419142b0c2307578d1318
Kushal Gautam <kushal.gautam@gmail.com>
2019-08-03 12:54:56 +0200

    added custom slf4j log implementations

---
+++ db-migrations/flyway-provider/src/main/java/com/example/infrastructure/Slf4jFlywayLogCreator.java
@@ -0,0 +1,11 @@
+import org.flywaydb.core.api.logging.Log;
+import org.flywaydb.core.api.logging.LogCreator;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jFlywayLogCreator implements LogCreator{
+
+    @Override
+    public Log createLogger(Class<?> clazz) {
+        return new Slf4jFlywayLog(LoggerFactory.getLogger(clazz));
+    }
+}
---
+++ db-migrations/flyway-provider/src/main/java/com/example/infrastructure/Slf4jFlywayLogger.java
@@ -0,0 +1,43 @@
+import com.example.db.infrastructure;
+
+import org.flywaydb.core.api.logging.Log;
+import org.slf4j.Logger;
+
+public class Slf4jFlywayLog implements Log{
+    
+    private final Logger logger;
+
+    public Slf4jFlywayLog(Logger logger) {
+        this.logger = logger;
+    }
+    
+    @Override
+    public boolean isDebugEnabled() {
+        return logger.isDebugEnabled();
+    }
+
+    @Override
+    public void debug(String message) {
+        logger.debug(message);
+    }
+
+    @Override
+    public void info(String message) {
+        logger.info(message);
+    }
+
+    @Override
+    public void warn(String message) {
+        logger.warn(message);
+    }
+
+    @Override
+    public void error(String message) {
+        logger.error(message);
+    }
+
+    @Override
+    public void error(String message, Exception exception) {
+        logger.error(message, exception);
+    }    
+}
